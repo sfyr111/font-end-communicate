ce07cc8cf5dba2767e19fc9037813ce3b186714d
sfyr111 <sfyr111@163.com>
2018-01-29 17:00:45 +0800

    jsonp cors websocket

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,33 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '跨域通信' });
 });
+
+// 跨域限制
+router.get('/someurl', function(req, res, next) {
+  res.send('somedata')
+})
+
+// JSONP
+router.get('/jsonp', function(req, res, next) {
+  const { callback } = req.query
+  const data = {
+    type: 'jsonp',
+    data: 'data'
+  }
+  res.send(callback + '(' + JSON.stringify(data) + ')')
+})
+
+// CORS
+router.options('/cors', function(req, res, next) {
+  // res.header('Access-Control-Allow-Origin', '*')
+  res.send('options ok')
+})
+router.post('/cors', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.send('cors ok')
+})
+
 
 module.exports = router;
